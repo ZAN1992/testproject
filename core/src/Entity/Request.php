9132a9d4979176d3eb4caaaf17de3b569e55aecf
Alex <kol9n2010@ya.ru>
2019-12-13 14:20:10 +0300

    Update request entity

--- core/src/Entity/Request.php
+++ core/src/Entity/Request.php
@@ -2,13 +2,33 @@
 
 class Request {
   private $request;
+  private $params;
 
-  public function set($request) {
-    $this->request = $request;
-    return $this;
+  private function __construct(array $params) {
+    $this->params = $params;
+    $this->parse();
   }
 
-  public function get() {
-    return $this->request;
+  public function getData() {
+    return $this->request['data'];
+  }
+
+  public static function create(array $params) {
+    return new self($params);
+  }
+
+  protected function parse() {
+    if (empty($this->params['type'])) {
+      // Session.
+    }
+    else {
+      if($this->params['type'] === 'login') {
+        $this->request['data']['login'] = $this->params['login'];
+        $this->request['data']['password'] = $this->params['password'];
+        $this->request['data']['email'] = $this->params['email'];
+        $this->request['data']['name'] = $this->params['name'];
+        $this->request['type'] = $this->params['type'];
+      }
+    }
   }
 }
